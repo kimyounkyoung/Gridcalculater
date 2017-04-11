0fcfff7ed6258b03f18932dff24ed247d2c2d945
kimyounkyoung <passposs2@naver.com>
2017-04-11 18:41:52 +0900

    #3 gridcalculater event function

--- app/src/main/java/passposs2/kr/hs/emirim/gridcalculater/MainActivity.java
+++ app/src/main/java/passposs2/kr/hs/emirim/gridcalculater/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int SELECT_EDIT2=1;
     int selectEdit=SELECT_EDIT1;
     String numStr="";
+    TextView textResult;
     //    int[] ids={R.id.but_0,R.id.but_1,R.id.but_2,R.id.but_3, R.id.but_4,
 //            R.id.but_5,R.id.but_6,R.id.but_7,R.id.but_8,R.id.but_9};
     @Override
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         edit2 = (EditText)findViewById(R.id.edit2);
         edit1.setOnTouchListener(editHandler);
         edit2.setOnTouchListener(editHandler);
+        textResult=(TextView)findViewById(R.id.text_result);
 
         for(int i=0;i<butNums.length;i++){
             butNums[i]=(Button)findViewById(R.id.but0+i);
@@ -32,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         for(int i=0;i<butOps.length;i++){
-            butOps[i]=(Button)findViewById(R.id.but_op1+i);
+            butOps[i]=(Button)findViewById(R.id.op1+i);
             butOps[i].setOnClickListener(butOpHandler);
         }
     }
@@ -55,7 +58,24 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener butOpHandler=new View.OnClickListener(){
         @Override
         public void onClick(View view) {
-
+            int num1=Integer.parseInt(edit1.getText().toString());
+            int num2=Integer.parseInt(edit2.getText().toString());
+            double result=0;
+            switch (view.getId()){
+                case R.id.op1:
+                    result=num1+num2;
+                    break;
+                case R.id.op2:
+                    result=num1-num2;
+                    break;
+                case R.id.op3:
+                    result=num1*num2;
+                    break;
+                case R.id.op4:
+                    result=(double)num1/num2;
+                    break;
+            }   //end switch
+            textResult.setText(result+"");
         }
     };
 
